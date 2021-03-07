bc64ce6091f77cc24f4fe05e5950690541bf78b6
afadeeva <alinarobertovna17.03@gmail.com>
2021-03-06 19:43:03 -0500

    fourth step

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import getUserName from '../src/cli.js';
+
 console.log('Welcome to the Brain Games');
-import getUserName from '/home/mfadeev/alina/hexlet-js/frontend-project-lvl1/src/cli.js';
 getUserName();
